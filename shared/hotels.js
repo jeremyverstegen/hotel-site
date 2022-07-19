2d824b343ab71a8ec06333e91e5bd3bee110cdcf
Jeremy Verstegen <jvmedia77@gmail.com>
2022-07-19 11:29:18 -0600

    started hotels array

---
+++ shared/hotels.js
@@ -0,0 +1,16 @@
+export const HOTELS = [
+    {
+        id: 0,
+        name: "Courtney's Palace",
+        image: "public/images/jake-irish-61OONDO7nrU-unsplash.jpg",
+        description: "Beatiful luxury huts in the Phillipines",
+        price: "320"
+    },
+    {
+        id: 1,
+        name: "Melmo Manner",
+        image: "public/images/roberto-nickson-MA82mPIZeGI-unsplash.jpg",
+        description: "Oceanside property",
+        price: "270"
+    }      
+]
